9c21bf4fe6a7ff8ce9312fcd8c058efbdd5d0d6e
sampittois <sam.pittois@gmail.com>
2025-11-23 14:20:53 +0100

    refactor index.php to remove welcome message and improve structure

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+require "Book.php";
+require "BookRepository.php";
+require "HomeController.php";
+
+// database connectie
+$pdo = new PDO(
+    "mysql:host=localhost;dbname=Bookshop;charset=utf8mb4",
+    "root",
+    ""
+);
+
+$bookRepo = new BookRepository($pdo);
+$controller = new HomeController($bookRepo);
+
+// laad de homepage
+$controller->index();
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +25,6 @@
     <title>Bookshop</title>
 </head>
 <body>
-    <h1>Welcome to this special Bookshop</h1>
+    
 </body>
 </html>
