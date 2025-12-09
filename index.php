fa834aea7b937b36dc639712c4670e524693b47f
sampittois <sam.pittois@gmail.com>
2025-12-09 10:36:48 +0100

    New start

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
-require "Book.php";
-require "BookRepository.php";
-require "CategoryRepository.php";
-require "AuthorRepository.php";
-require "HomeController.php";
+    $username = 
 
-// Database connection
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=bookshop;charset=utf8mb4",
-    "root",
-    ""
-);
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookshop</title>
+</head>
+<body>
+    <h1>Library</h1>
 
-$bookRepo = new BookRepository($pdo);
-$categoryRepo = new CategoryRepository($pdo);
-$authorRepo = new AuthorRepository($pdo);
-
-$controller = new HomeController($bookRepo, $categoryRepo, $authorRepo);
-
-// Load the homepage
-$controller->index();
+</body>
+</html>
